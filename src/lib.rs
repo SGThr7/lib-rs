688c896604749aaf67b26dc53db621fd0c4a268f
SGThr7 <sgthr7@gmail.com>
2021-08-19 22:32:11 +0900

    refactor: Move file

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,5 @@
 pub mod macros;
 #[macro_use]
 pub mod math;
-pub mod binary_indexed_tree;
 pub mod structs;
 pub mod tools;
--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,2 +1,3 @@
+pub mod binary_indexed_tree;
 pub mod segment_tree;
 pub mod union_find;
