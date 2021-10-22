80ada152f71dd9b42eee0d61efcdb8a07bbd7725
SGThr7 <sgthr7@gmail.com>
2021-10-22 11:09:25 +0900

    feat: Similar to std::ops

--- src/math/gcd_lcm.rs
+++ src/math/gcd_lcm.rs
@@ -1,7 +1,8 @@
 #[codesnip::entry("GCD_LCM")]
-pub trait CommonNum {
-    fn gcd(&self, other: &Self) -> Self;
-    fn lcm(&self, other: &Self) -> Self;
+pub trait CommonNum<Rhs = Self> {
+    type Output;
+    fn gcd(self, other: Rhs) -> Self::Output;
+    fn lcm(self, other: Rhs) -> Self::Output;
 }
 
 #[codesnip::entry("GCD_LCM")]
@@ -9,13 +10,32 @@ mod common_num_impl {
     use super::CommonNum;
 
     macro_rules! impl_common_num {
+        (@forward_ref $t:ty) => {
+            impl CommonNum<&$t> for $t {
+                type Output = <$t as CommonNum>::Output;
+                fn gcd(self, other: &$t) -> Self::Output { self.gcd(*other) }
+                fn lcm(self, other: &$t) -> Self::Output { self.lcm(*other) }
+            }
+            impl CommonNum<$t> for &$t {
+                type Output = <$t as CommonNum>::Output;
+                fn gcd(self, other: $t) -> Self::Output { self.clone().gcd(other) }
+                fn lcm(self, other: $t) -> Self::Output { self.clone().lcm(other) }
+            }
+            impl CommonNum<&$t> for &$t {
+                type Output = <$t as CommonNum>::Output;
+                fn gcd(self, other: &$t) -> Self::Output { self.clone().gcd(other.clone()) }
+                fn lcm(self, other: &$t) -> Self::Output { self.clone().lcm(other.clone()) }
+            }
+        };
         ($zero:expr, for $($t:ty)*) => {$(
             impl CommonNum for $t {
-                fn gcd(&self, other: &Self) -> Self {
+                type Output = $t;
+
+                fn gcd(self, other: Self) -> Self {
                     let (mut a, mut b) = if self >= other {
-                        (*self, *other)
+                        (self, other)
                     } else {
-                        (*other, *self)
+                        (other, self)
                     };
                     let mut r = a.rem_euclid(b);
                     while r > $zero {
@@ -26,11 +46,12 @@ mod common_num_impl {
                     a.div_euclid(b)
                 }
 
-                fn lcm(&self, other: &Self) -> Self {
+                fn lcm(self, other: Self) -> Self {
                     let gcd = self.gcd(other);
                     self / gcd * other
                 }
             }
+            impl_common_num! { @forward_ref $t }
         )*};
     }
 
