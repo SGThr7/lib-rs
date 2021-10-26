377dd886a8c52343942ad00df99cbc3fdb2c67dd
SGThr7 <sgthr7@gmail.com>
2021-10-26 13:56:52 +0900

    refactor: Replace macro to generics pattern

---
+++ src/math/modint/factmap.rs
@@ -0,0 +1,213 @@
+use super::{ModInt, ModInt1e9_7, ModInt998244353, Modulo};
+
+#[codesnip::entry("ModIntFactMap1e9_7", include("ModIntFactMap", "ModInt1e9_7"))]
+pub type FactMap1e9_7 = ModIntFactMap<ModInt1e9_7>;
+
+#[codesnip::entry("ModIntFactMap998244353", include("ModIntFactMap", "ModInt998244353"))]
+pub type FactMap998244353 = ModIntFactMap<ModInt998244353>;
+
+#[codesnip::entry("ModIntFactMap")]
+pub use modint_factmap_impl::ModIntFactMap;
+
+#[codesnip::entry("ModIntFactMap", include("ModInt", "Modulo"))]
+mod modint_factmap_impl {
+    use super::{ModInt, Modulo};
+    use core::fmt;
+
+    type Set = usize;
+
+    #[derive(Clone)]
+    pub struct ModIntFactMap<M> {
+        len: usize,
+        recip: Vec<ModInt<M>>,
+        factorial: Vec<ModInt<M>>,
+        recip_fact: Vec<ModInt<M>>,
+    }
+
+    impl<M> ModIntFactMap<M> {
+        const MIN_LEN: usize = 1;
+
+        pub fn len(&self) -> usize {
+            self.len
+        }
+
+        pub fn get_recip(&self, x: usize) -> Option<&ModInt<M>> {
+            self.recip.get(x)
+        }
+
+        pub fn get_factorial(&self, x: usize) -> Option<&ModInt<M>> {
+            self.factorial.get(x)
+        }
+
+        pub fn get_recip_fact(&self, x: usize) -> Option<&ModInt<M>> {
+            self.recip_fact.get(x)
+        }
+    }
+
+    impl<M: Clone + Modulo<Set = Set>> ModIntFactMap<M> {
+        pub fn new(size: usize) -> Self {
+            let len = size.max(Self::MIN_LEN) + 1;
+
+            let mut recip = Vec::with_capacity(len);
+            recip.push(ModInt::<M>::zero());
+            recip.push(ModInt::<M>::one());
+
+            let mut factorial = Vec::with_capacity(len);
+            factorial.push(ModInt::<M>::one());
+            factorial.push(ModInt::<M>::one());
+
+            let mut recip_fact = Vec::with_capacity(len);
+            recip_fact.push(ModInt::<M>::one());
+            recip_fact.push(ModInt::<M>::one());
+
+            let mut ret = Self {
+                len: 2,
+                recip,
+                factorial,
+                recip_fact,
+            };
+            ret.increase(len - 2);
+            ret
+        }
+
+        pub fn increase(&mut self, additional: usize) {
+            assert!(self.len() + additional - 1 <= M::MOD);
+            self.recip.reserve_exact(additional);
+            self.factorial.reserve_exact(additional);
+            self.recip_fact.reserve_exact(additional);
+
+            for d in 0..additional {
+                let i = self.len() + d;
+                // P = ⌊P/i⌋×i + P%i
+                // 0 ≡ ⌊P/i⌋×i + P%i (mod P)
+                // 0 ≡ ⌊P/i⌋ + (P%i)×i⁻¹ (mod P)
+                // -⌊P/i⌋ ≡ (P%i)×i⁻¹ (mod P)
+                // i⁻¹ ≡ -⌊P/i⌋ × (P%i)⁻¹ (mod P)
+                // i⁻¹ ≡ -(P%i)⁻¹ × ⌊P/i⌋ (mod P)
+                self.recip
+                    .push(-self.recip[M::MOD % i].clone() * (M::MOD / i));
+                self.factorial.push(self.factorial[i - 1].clone() * i);
+                self.recip_fact
+                    .push(self.recip_fact[i - 1].clone() * self.recip[i].clone());
+            }
+            self.len += additional;
+        }
+
+        pub fn increase_to(&mut self, x: usize) {
+            if x >= self.len() {
+                self.increase(x + 1 - self.len())
+            }
+        }
+
+        pub fn recip(&mut self, x: usize) -> ModInt<M> {
+            if x < self.len() {
+                self.recip[x].clone()
+            } else {
+                self.increase_to(x);
+                self.recip[x].clone()
+            }
+        }
+
+        pub fn factorial(&mut self, x: usize) -> ModInt<M> {
+            if x < self.len() {
+                self.factorial[x].clone()
+            } else {
+                self.increase_to(x);
+                self.factorial[x].clone()
+            }
+        }
+
+        pub fn recip_fact(&mut self, x: usize) -> ModInt<M> {
+            if x < self.len() {
+                self.recip_fact[x].clone()
+            } else {
+                self.increase_to(x);
+                self.recip_fact[x].clone()
+            }
+        }
+
+        pub fn get_combination(&self, n: usize, k: usize) -> Option<ModInt<M>> {
+            if n < k {
+                None
+            } else if k == 0 || n == k {
+                Some(ModInt::<M>::one())
+            } else {
+                let ni = self.get_factorial(n);
+                let kii = self.get_recip_fact(k);
+                let nkii = self.get_recip_fact(n - k);
+                if let (Some(ni), Some(kii), Some(nkii)) =
+                    (ni.cloned(), kii.cloned(), nkii.cloned())
+                {
+                    Some(ni * kii * nkii)
+                } else {
+                    None
+                }
+            }
+        }
+
+        pub fn combination(&mut self, n: usize, k: usize) -> ModInt<M> {
+            assert!(n >= k);
+            let ni = self.factorial(n);
+            let kii = self.recip_fact(k);
+            let nkii = self.recip_fact(n - k);
+
+            ni * kii * nkii
+        }
+
+        pub fn get_multi_choose(&self, n: usize, k: usize) -> Option<ModInt<M>> {
+            self.get_combination(n + k - 1, k)
+        }
+
+        pub fn multi_choose(&mut self, n: usize, k: usize) -> ModInt<M> {
+            self.combination(n + k - 1, k)
+        }
+    }
+
+    impl<M: fmt::Debug> fmt::Debug for ModIntFactMap<M> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            f.debug_struct("ModIntFactMap")
+                .field("len", &self.len)
+                .field("recip", &self.recip)
+                .field("factorial", &self.factorial)
+                .field("recip_fact", &self.recip_fact)
+                .finish()
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::{ModIntFactMap, Modulo};
+
+        #[derive(Clone, Copy, Debug)]
+        enum ModuloTest {}
+        impl Modulo for ModuloTest {
+            type Set = usize;
+            const MOD: Self::Set = 13;
+        }
+        type Map = ModIntFactMap<ModuloTest>;
+
+        fn fact(x: usize) -> usize {
+            (1..=x).product()
+        }
+
+        fn comb(n: usize, k: usize) -> usize {
+            fact(n) / fact(k) / fact(n - k)
+        }
+
+        #[test]
+        fn combination() {
+            let max = 11;
+            let map = Map::new(max);
+
+            for i in 0..=max {
+                assert_eq!(
+                    map.get_combination(max, i).unwrap(),
+                    comb(max, i) % ModuloTest::MOD,
+                    "{}C{}",
+                    max,
+                    i
+                );
+            }
+        }
+    }
+}
--- src/math/modint/modulo.rs
+++ src/math/modint/modulo.rs
@@ -4,16 +4,18 @@ pub trait Modulo {
     const MOD: Self::Set;
 }
 
-#[codesnip::entry("Modulo1000000007")]
-pub struct Modulo1000000007;
-#[codesnip::entry("Modulo1000000007", include("Modulo"))]
-impl Modulo for Modulo1000000007 {
+#[codesnip::entry("Modulo1e9_7")]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Modulo1e9_7 {}
+#[codesnip::entry("Modulo1e9_7", include("Modulo"))]
+impl Modulo for Modulo1e9_7 {
     type Set = usize;
     const MOD: Self::Set = 1_000_000_007;
 }
 
 #[codesnip::entry("Modulo998244353")]
-pub struct Modulo998244353;
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Modulo998244353 {}
 #[codesnip::entry("Modulo998244353", include("Modulo"))]
 impl Modulo for Modulo998244353 {
     type Set = usize;
