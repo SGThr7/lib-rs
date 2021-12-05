e82821115ea9c7eec0b79d643fd5b1020f310c76
SGThr7 <sgthr7@gmail.com>
2021-12-05 15:32:52 +0900

    refactor: Rename abs_sub to abs_diff
    matched to std library
    https://doc.rust-lang.org/nightly/std/primitive.usize.html#method.abs_diff

--- src/tools/abs_sub.rs
+++ src/tools/abs_sub.rs
@@ -1,17 +1,20 @@
 #[codesnip::entry("AbsSub")]
-pub use abs_sub_impl::AbsSub;
+pub use abs_diff_impl::AbsDiff;
 
 #[codesnip::entry("AbsSub")]
-mod abs_sub_impl {
-    pub trait AbsSub {
-        fn abs_sub(&self, other: &Self) -> Self;
+mod abs_diff_impl {
+    pub trait AbsDiff<Rhs = Self> {
+        type Output;
+        fn abs_diff(self, other: Self) -> Self;
     }
 
     macro_rules! impl_abs_sub {
         ($($t:ty)*) => ($(
-            impl AbsSub for $t {
-                fn abs_sub(&self, other: &Self) -> Self {
-                    if self > other {
+            impl AbsDiff for $t {
+                type Output = Self;
+
+                fn abs_diff(self, other: Self) -> Self {
+                    if self >= other {
                         self - other
                     } else {
                         other - self
@@ -21,5 +24,16 @@ mod abs_sub_impl {
         )*)
     }
 
-    impl_abs_sub!(u8 u16 u32 u64 u128 usize);
+    impl_abs_sub! { u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize f32 f64 }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn abs_diff() {
+        assert_eq!(10.abs_diff(20), 10);
+        assert_eq!(100.abs_diff(20), 80);
+    }
 }
