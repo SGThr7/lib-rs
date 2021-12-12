88ae5341b0d73b6f738274f85fca96ad383ff7c7
SGThr7 <sgthr7@gmail.com>
2021-12-12 11:52:04 +0900

    refactor: Add docs and simplify impls

--- src/structs.rs
+++ src/structs.rs
@@ -1,3 +1,9 @@
+use crate::math::num::Monoid;
+#[cfg_attr(nightly, codesnip::entry("BinaryIndexedTree", include("Monoid")))]
 pub mod binary_indexed_tree;
+#[codesnip::entry("BinaryIndexedTree")]
+#[allow(unused_imports)]
+use binary_indexed_tree::{BinaryIndexedTree, BIT};
+
 pub mod segment_tree;
 pub mod union_find;
--- src/structs/binary_indexed_tree.rs
+++ src/structs/binary_indexed_tree.rs
@@ -1,346 +1,150 @@
-use crate::math::num::Monoid;
+use super::Monoid;
 
-#[codesnip::entry("BinaryIndexedTree", include("Monoid"))]
-pub use binary_indexed_tree::BinaryIndexedTree;
+use std::ops::{RangeFull, RangeTo, RangeToInclusive};
 
-#[codesnip::entry("BinaryIndexedTree", include("Monoid"))]
-mod binary_indexed_tree {
-    use super::Monoid;
-    use core::{
-        fmt::Debug,
-        iter::FusedIterator,
-        ops::{RangeFull, RangeTo, RangeToInclusive},
-        slice,
-    };
-    use std::vec;
+/// A data structure that can efficiently operate elements and calculate prefix folds in a vec.
+///
+/// `BinaryIndexedTree` can be used with any algebraic structure that implements [`Monoid`].
+///
+/// A `BinaryIndexedTree` can be initialized from a vec.
+///
+/// ```
+/// use lib_rust::structs::binary_indexed_tree::BinaryIndexedTree;
+/// use lib_rust::math::num::alge_struct::monoid::AddMonoid;
+///
+/// let bit = BinaryIndexedTree::<AddMonoid<_>>::from(vec![1, 2, 3]);
+/// ```
+///
+/// # Time complexity
+///
+/// | Algorithm   | Average     | Worst case  |
+/// | ----------- | ----------- | ----------- |
+/// | Memory      | O(*n*)      | O(*n*)      |
+/// | [`fold`]    | O(log(*n*)) | O(log(*n*)) |
+/// | [`operate`] | O(log(*n*)) | O(log(*n*)) |
+///
+/// [`fold`]: BinaryIndexedTree::fold
+/// [`operate`]: BinaryIndexedTree::operate
+pub struct BinaryIndexedTree<T: Monoid> {
+    tree: Vec<T::Set>,
+}
 
-    #[derive(Clone, PartialEq, Eq)]
-    pub struct BinaryIndexedTree<T: Monoid> {
-        tree: Vec<T::Set>,
+/// Type alias for [`BinaryIndexedTree`].
+pub type BIT<T> = BinaryIndexedTree<T>;
+
+/// Returns the least significant bit by `i`.
+fn lsb(i: usize) -> usize {
+    i & i.wrapping_neg()
+}
+
+#[cfg(test)]
+#[test]
+fn lsb_test() {
+    for i in 1..=(1e7 as usize) {
+        let t = lsb(i);
+        assert_eq!(t.trailing_zeros(), i.trailing_zeros());
+        assert_eq!(t.count_ones(), 1);
     }
+}
 
-    impl<T: Monoid> BinaryIndexedTree<T> {
-        pub fn new() -> Self {
-            Self { tree: vec![] }
-        }
-
-        pub fn with_size(size: usize) -> Self {
-            Self {
-                tree: vec![T::id(); size],
-            }
-        }
-
-        pub fn len(&self) -> usize {
-            self.tree.len()
-        }
-
-        pub fn capacity(&self) -> usize {
-            self.tree.len()
-        }
-
-        pub fn push(&mut self, value: T::Set) {
-            self.tree
-                .push(T::operate(&unsafe { self.get_unchecked(..) }, &value))
-        }
-
-        pub fn increase(&mut self, additional: usize) {
-            self.tree.reserve(additional);
-            for _ in 0..additional {
-                self.push(T::id());
-            }
-        }
-
-        pub fn get<Q: BITQuery<T>>(&self, query: Q) -> Option<Q::Output> {
-            query.query(self)
-        }
-
-        pub unsafe fn get_unchecked<Q: BITQuery<T>>(&self, query: Q) -> Q::Output {
-            query.query_unchecked(self)
-        }
-
-        pub fn operate(&mut self, index: usize, value: &T::Set) {
-            let mut i = index;
-            while i < self.len() {
-                self.tree[i] = T::operate(value, &self.tree[i]);
-                i += lsb(i + 1);
-            }
-        }
-
-        pub fn iter<'a>(&'a self) -> Iter<'a, T> {
-            Iter::new(self)
-        }
-
-        pub fn lower_bound(&self, x: T::Set) -> usize
-        where
-            T::Set: PartialOrd,
-        {
-            let mut len = self.len().next_power_of_two();
-            let mut i = 0;
-            let mut total = T::id();
-            while 0 < len {
-                if i + len - 1 < self.len() && T::operate(&total, &self.tree[i + len - 1]) < x {
-                    total = T::operate(&total, &self.tree[i + len - 1]);
-                    i += len;
-                }
-                len >>= 1;
-            }
-            i
+impl<T: Monoid> BinaryIndexedTree<T> {
+    /// Creates an initialized tree with `Monoid::id()`.
+    pub fn with_size(size: usize) -> Self {
+        Self {
+            tree: vec![T::id(); size],
         }
     }
 
-    impl<T> Debug for BinaryIndexedTree<T>
-    where
-        T: Monoid,
-        T::Set: Debug,
-    {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            f.debug_list().entries(self.iter()).finish()
+    /// Returns the number of elements in the tree.
+    pub fn len(&self) -> usize {
+        self.tree.len()
+    }
+
+    /// Returns a folded value.
+    /// The `index` is allowed with [`RangeTo`], [`RangeToInclusive`] or [`RangeFull`].
+    ///
+    /// This operation is O(log(*n*)).
+    ///
+    /// # Panics
+    ///
+    /// May panic if the range is out of bounds.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use lib_rust::structs::binary_indexed_tree::BinaryIndexedTree;
+    /// use lib_rust::math::num::alge_struct::monoid::AddMonoid;
+    ///
+    /// let bit = BinaryIndexedTree::<AddMonoid<_>>::from(vec![1, 2, 3, 4]);
+    /// assert_eq!(bit.fold(..2), 3);
+    /// assert_eq!(bit.fold(..=2), 6);
+    /// assert_eq!(bit.fold(..), 10);
+    /// ```
+    pub fn fold<I: BITIndex<T>>(&self, index: I) -> T::Set {
+        index.fold(self)
+    }
+
+    /// Update vec value with [`Semigroup::operate`].
+    ///
+    /// This operation is O(log(*n*)).
+    ///
+    /// [`Semigroup::operate`]: crate::math::num::Semigroup
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use lib_rust::structs::binary_indexed_tree::BinaryIndexedTree;
+    /// use lib_rust::math::num::alge_struct::monoid::AddMonoid;
+    ///
+    /// let mut bit = BinaryIndexedTree::<AddMonoid<_>>::from(vec![1, 2, 3, 4]);
+    /// bit.operate(2, &5);
+    /// assert_eq!(bit.fold(..), 15);
+    /// ```
+    pub fn operate(&mut self, index: usize, value: &T::Set) {
+        let mut i = index;
+        while i < self.len() {
+            self.tree[i] = T::operate(value, &self.tree[i]);
+            i += lsb(i + 1);
         }
     }
+}
 
-    impl<T: Monoid> Default for BinaryIndexedTree<T> {
-        fn default() -> Self {
-            Self::new()
-        }
-    }
+pub trait BITIndex<T: Monoid> {
+    fn fold(self, bit: &BinaryIndexedTree<T>) -> T::Set;
+}
 
-    impl<T> From<Vec<T::Set>> for BinaryIndexedTree<T>
-    where
-        T: Monoid,
-    {
-        fn from(v: Vec<T::Set>) -> Self {
-            let mut res = Self::with_size(v.len());
-            for (i, x) in v.into_iter().enumerate() {
-                res.operate(i, &x.clone().into());
-            }
-            res
-        }
-    }
-
-    /// Least Significant Bit
-    fn lsb(i: usize) -> usize {
-        i & i.wrapping_neg()
-    }
-
-    pub fn query<T: Monoid>(bit: &BinaryIndexedTree<T>, exclusive_bound: usize) -> Option<T::Set> {
-        if exclusive_bound <= bit.len() {
-            Some(unsafe { query_unchecked(bit, exclusive_bound) })
-        } else {
-            None
-        }
-    }
-
-    pub unsafe fn query_unchecked<T: Monoid>(
-        bit: &BinaryIndexedTree<T>,
-        exclusive_bound: usize,
-    ) -> T::Set {
+impl<T: Monoid> BITIndex<T> for RangeTo<usize> {
+    fn fold(self, bit: &BinaryIndexedTree<T>) -> T::Set {
         let mut ret = T::id();
-        let mut i = exclusive_bound;
+        let mut i = self.end;
         while 0 < i {
             ret = T::operate(&bit.tree[i - 1], &ret);
             i -= lsb(i);
         }
         ret
     }
+}
 
-    pub trait BITQuery<T: Monoid> {
-        type Output: Clone;
-        fn query(self, bit: &BinaryIndexedTree<T>) -> Option<Self::Output>;
-        unsafe fn query_unchecked(self, bit: &BinaryIndexedTree<T>) -> Self::Output;
-    }
-
-    impl<T: Monoid> BITQuery<T> for RangeTo<usize> {
-        type Output = T::Set;
-
-        fn query(self, bit: &BinaryIndexedTree<T>) -> Option<Self::Output> {
-            query(bit, self.end)
-        }
-        unsafe fn query_unchecked(self, bit: &BinaryIndexedTree<T>) -> Self::Output {
-            query_unchecked(bit, self.end)
-        }
-    }
-
-    impl<T: Monoid> BITQuery<T> for RangeToInclusive<usize> {
-        type Output = T::Set;
-
-        fn query(self, bit: &BinaryIndexedTree<T>) -> Option<Self::Output> {
-            query(bit, self.end + 1)
-        }
-        unsafe fn query_unchecked(self, bit: &BinaryIndexedTree<T>) -> Self::Output {
-            query_unchecked(bit, self.end + 1)
-        }
-    }
-
-    impl<T: Monoid> BITQuery<T> for RangeFull {
-        type Output = T::Set;
-
-        fn query(self, bit: &BinaryIndexedTree<T>) -> Option<Self::Output> {
-            (..bit.len()).query(bit)
-        }
-        unsafe fn query_unchecked(self, bit: &BinaryIndexedTree<T>) -> Self::Output {
-            (..bit.len()).query_unchecked(bit)
-        }
-    }
-
-    pub struct IntoIter<T: Monoid> {
-        iter: vec::IntoIter<T::Set>,
-        index: usize,
-        memo: Vec<T::Set>,
-    }
-
-    impl<T: Monoid> Iterator for IntoIter<T> {
-        type Item = T::Set;
-        fn next(&mut self) -> Option<Self::Item> {
-            if let Some(value) = self.iter.next() {
-                let i = self.index + 1;
-                if i.is_power_of_two() {
-                    // 2^nの時はlsb(index)がmemo.len()より大きいからpushする
-                    self.memo.push(value);
-                } else {
-                    // 古いのは使わないので上書き
-                    self.memo[lsb(i).trailing_zeros() as usize] = value;
-                }
-                self.index += 1;
-
-                let mut i = i;
-                let mut ret = T::id();
-                while 0 < i {
-                    ret = T::operate(&self.memo[i.trailing_zeros() as usize], &ret);
-                    i -= lsb(i);
-                }
-                Some(ret)
-            } else {
-                None
-            }
-        }
-    }
-
-    impl<T: Monoid> FusedIterator for IntoIter<T> {}
-
-    impl<T: Monoid> IntoIterator for BinaryIndexedTree<T> {
-        type Item = T::Set;
-        type IntoIter = IntoIter<T>;
-        fn into_iter(self) -> Self::IntoIter {
-            IntoIter {
-                iter: self.tree.into_iter(),
-                index: 0,
-                memo: vec![],
-            }
-        }
-    }
-
-    pub struct Iter<'a, T: Monoid> {
-        iter: slice::Iter<'a, T::Set>,
-        index: usize,
-        memo: Vec<&'a T::Set>,
-    }
-
-    impl<'a, T: Monoid> Iter<'a, T> {
-        pub fn new(bit: &'a BinaryIndexedTree<T>) -> Self {
-            Self {
-                iter: bit.tree.iter(),
-                index: 0,
-                memo: vec![],
-            }
-        }
-    }
-
-    impl<'a, T: Monoid> Iterator for Iter<'a, T> {
-        type Item = T::Set;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            if let Some(value) = self.iter.next() {
-                let i = self.index + 1;
-                if i.is_power_of_two() {
-                    // 2^nの時はlsb(index)がmemo.len()より大きいからpushする
-                    self.memo.push(value);
-                } else {
-                    // 古いのは使わないので上書き
-                    self.memo[lsb(i).trailing_zeros() as usize] = value;
-                }
-                self.index += 1;
-
-                let mut i = i;
-                let mut ret = T::id();
-                while 0 < i {
-                    ret = T::operate(self.memo[i.trailing_zeros() as usize], &ret);
-                    i -= lsb(i);
-                }
-                Some(ret)
-            } else {
-                None
-            }
-        }
-    }
-
-    impl<'a, T: Monoid> FusedIterator for Iter<'a, T> {}
-
-    #[cfg(test)]
-    #[test]
-    fn lsb_test() {
-        for i in 1..(1e7 as usize) {
-            let t = super::binary_indexed_tree::lsb(i);
-            assert_eq!(i.trailing_zeros(), t.trailing_zeros());
-            assert_eq!(1, t.count_ones());
-        }
+impl<T: Monoid> BITIndex<T> for RangeToInclusive<usize> {
+    fn fold(self, bit: &BinaryIndexedTree<T>) -> T::Set {
+        let to = ..self.end + 1;
+        to.fold(bit)
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::math::num::alge_struct::monoid::AddMonoid;
-    type BITree<T> = BinaryIndexedTree<AddMonoid<T>>;
-
-    #[test]
-    fn fmt_debug() {
-        let bit = BITree::from(vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
-        // [1, 3, 6, 10, 15, 21, 28, 36, 45]
-        println!("{:?}", bit);
+impl<T: Monoid> BITIndex<T> for RangeFull {
+    fn fold(self, bit: &BinaryIndexedTree<T>) -> T::Set {
+        let to = ..bit.len();
+        to.fold(bit)
     }
+}
 
-    #[test]
-    fn get() {
-        let bit = BITree::from(vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
-        assert_eq!(
-            vec![0, 1, 3, 6, 10, 15, 21, 28, 36, 45],
-            (0..=bit.len())
-                .map(|i| bit.get(..i).unwrap())
-                .collect::<Vec<_>>()
-        );
-    }
-
-    #[test]
-    fn into_iter() {
-        let bit = BITree::from(vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
-        assert_eq!(
-            vec![1, 3, 6, 10, 15, 21, 28, 36, 45],
-            bit.into_iter().collect::<Vec<_>>()
-        );
-    }
-
-    #[test]
-    fn iter() {
-        let bit = BITree::from(vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
-        assert_eq!(
-            vec![1, 3, 6, 10, 15, 21, 28, 36, 45],
-            bit.iter().collect::<Vec<_>>()
-        );
-    }
-
-    #[test]
-    fn lower_bound() {
-        let t = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
-        let ans = {
-            vec![0]
-                .into_iter()
-                .chain(t.iter().enumerate().map(|(i, &x)| vec![i; x]).flatten())
-                .chain(vec![t.len(); 2])
-                .collect::<Vec<_>>()
-        };
-        let bit = BITree::from(t);
-        for (i, ans) in ans.into_iter().enumerate() {
-            assert_eq!(ans, bit.lower_bound(i), "i={}", i);
-        }
+impl<T: Monoid> From<Vec<T::Set>> for BinaryIndexedTree<T> {
+    fn from(v: Vec<T::Set>) -> Self {
+        let mut ret = Self::with_size(v.len());
+        v.into_iter()
+            .enumerate()
+            .for_each(|(i, x)| ret.operate(i, &x));
+        ret
     }
 }
