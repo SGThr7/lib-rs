cfe552c363914d1584e9025b176c0f6e76b27cd1
SGThr7 <sgthr7@gmail.com>
2021-12-25 19:27:26 +0900

    style: remove example

--- crates/unionfind/src/lib.rs
+++ crates/unionfind/src/lib.rs
@@ -2,35 +2,6 @@
 ///
 /// - **Union**: merge two sets.
 /// - **Find**: find a representative member of a set.
-///
-/// # Examples
-///
-/// [AtCoder Typical Contest 001 B - Union Find](https://atcoder.jp/contests/atc001/tasks/unionfind_a)
-///
-/// ```no_run
-/// use proconio::input;
-/// use sgthr7_lib_unionfind::UnionFind;
-///
-/// fn main() {
-///     input!(n: usize, query: [(u8, usize, usize)]);
-///
-///     let mut uf = UnionFind::new(n);
-///     for (mode, a, b) in query {
-///         match mode {
-///             0 => uf.union(a, b),
-///             1 => {
-///                 if uf.equiv(a, b) {
-///                     println!("Yes")
-///                 } else {
-///                     println!("No")
-///                 }
-///             }
-///             _ => unreachable!(),
-///         }
-///     }
-/// }
-/// ```
-///
 pub struct UnionFind {
     parent: Vec<usize>,
     size: Vec<usize>,
