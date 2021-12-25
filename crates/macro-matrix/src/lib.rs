981af53b86854f5cb52cbef0f878f00cd503553d
SGThr7 <sgthr7@gmail.com>
2021-12-25 19:57:29 +0900

    feat: migrate matrix macro

---
+++ crates/macro-matrix/src/lib.rs
@@ -0,0 +1,25 @@
+/// Creates a multi dimention [`Vec`].
+///
+/// # Examples
+///
+/// ```
+/// use sgthr7_lib_macro_matrix::mat;
+///
+/// let m = mat![1; 2, 3];
+///
+/// assert_eq!(m.len(), 2);
+/// assert!(m.iter().all(|v| v.len() == 3));
+/// assert!(m.iter().flatten().all(|&x| x == 1));
+/// ```
+#[macro_export]
+macro_rules! mat {
+    () => {
+        vec![]
+    };
+    ($e:expr; $n:expr $(,)?) => {
+        vec![$e; $n]
+    };
+    ($e:expr; $nhead:expr, $($ntail:expr),* $(,)?) => {
+        vec![$crate::mat![$e; $($ntail),*]; $nhead]
+    };
+}
