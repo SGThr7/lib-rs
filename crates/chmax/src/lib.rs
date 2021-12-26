280285581ca0b6fd89046ccce4171cf0d90bfb56
SGThr7 <sgthr7@gmail.com>
2021-12-26 18:05:25 +0900

    feat: Migrate chmin, chmax

---
+++ crates/chmax/src/lib.rs
@@ -0,0 +1,21 @@
+/// Replace with maximum one.
+///
+/// # Examples
+///
+/// ```
+/// use sgthr7_lib_chmax::chmax;
+///
+/// let mut x = 10;
+///
+/// chmax!(x, 5);
+/// assert_eq!(x, 10);
+///
+/// chmax!(x, 20);
+/// assert_eq!(x, 20);
+/// ```
+#[macro_export]
+macro_rules! chmax {
+    ($lhs:expr, $rhs:expr) => {
+        $lhs = $lhs.max($rhs)
+    };
+}
---
+++ crates/chmin/src/lib.rs
@@ -0,0 +1,21 @@
+/// Replace with minimam one.
+///
+/// # Examples
+///
+/// ```
+/// use sgthr7_lib_chmin::chmin;
+///
+/// let mut x = 10;
+///
+/// chmin!(x, 20);
+/// assert_eq!(x, 10);
+///
+/// chmin!(x, 5);
+/// assert_eq!(x, 5);
+/// ```
+#[macro_export]
+macro_rules! chmin {
+    ($lhs:expr, $rhs:expr) => {
+        $lhs = $lhs.min($rhs)
+    };
+}
