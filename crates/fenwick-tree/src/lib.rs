59f48e75f72538c0fb71384189ed59c10c562bf0
SGThr7 <sgthr7@gmail.com>
2022-02-27 23:55:14 +0900

    feat: Impl bisect for fenwick tree

--- crates/fenwick-tree/src/lib.rs
+++ crates/fenwick-tree/src/lib.rs
@@ -1,8 +1,10 @@
 use std::{
+    cmp::Ordering,
     mem::replace,
-    ops::{RangeFull, RangeTo, RangeToInclusive},
+    ops::{Range, RangeFull, RangeTo, RangeToInclusive},
 };
 
+use bisect::Bisect;
 use monoid::Monoid;
 
 /// A struct that can update elements and calculate prefix sums fast.
@@ -147,5 +149,105 @@ impl<T: Monoid> Index<T> for RangeToInclusive<usize> {
 impl<T: Monoid> Index<T> for RangeFull {
     fn fold(self, tree: &FenwickTree<T>) -> <T>::Set {
         (..tree.len()).fold(tree)
+    }
+}
+
+impl<T: Monoid> Bisect for FenwickTree<T> {
+    type Item = T::Set;
+
+    fn find_range_by<F>(&self, mut f: F) -> Range<usize>
+    where
+        F: FnMut(&Self::Item) -> Ordering,
+    {
+        let mut len = self.len().next_power_of_two();
+        let mut i = 0;
+        let mut total = T::id();
+
+        while len > 0 {
+            if i + len - 1 < self.len() {
+                let tmp = T::operate(total.clone(), self.tree[i + len - 1].clone());
+                let cmp = f(&tmp);
+                match cmp {
+                    Ordering::Less => {
+                        i += len;
+                        total = tmp;
+                    }
+                    Ordering::Greater => (),
+                    Ordering::Equal => break,
+                }
+            }
+
+            len /= 2;
+        }
+
+        let mut lower_i = i;
+        let mut lower_total = total.clone();
+        let mut upper_i = i;
+        let mut upper_total = total;
+        while len > 0 {
+            let lower_tmp = T::operate(lower_total.clone(), self.tree[i + len - 1].clone());
+            let lower_cmp = f(&lower_tmp);
+            match lower_cmp {
+                Ordering::Less => {
+                    lower_i += len;
+                    lower_total = lower_tmp;
+                }
+                Ordering::Equal | Ordering::Greater => (),
+            }
+
+            let upper_tmp = T::operate(upper_total.clone(), self.tree[i + len - 1].clone());
+            let upper_cmp = f(&upper_tmp);
+            match upper_cmp {
+                Ordering::Equal | Ordering::Less => {
+                    upper_i += len;
+                    upper_total = upper_tmp;
+                }
+                Ordering::Greater => (),
+            }
+
+            len /= 2;
+        }
+
+        lower_i..upper_i
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use monoid::types::AddMonoid;
+
+    #[test]
+    fn find_range_by() {
+        let bit: FenwickTree<AddMonoid<_>> = vec![1, 2, 3, 4, 5, 6, 6, 8, 9, 10].into();
+
+        assert_eq!(bit.find_range_by(|x| x.cmp(&1)), 0..1);
+        assert_eq!(bit.find_range_by(|x| x.cmp(&2)), 1..1);
+        assert_eq!(bit.find_range_by(|x| x.cmp(&3)), 1..2);
+        (4..=5).for_each(|i| assert_eq!(bit.find_range_by(|x| x.cmp(&i)), 2..2));
+        assert_eq!(bit.find_range_by(|x| x.cmp(&6)), 2..3);
+        (7..=9).for_each(|i| assert_eq!(bit.find_range_by(|x| x.cmp(&i)), 3..3));
+        assert_eq!(bit.find_range_by(|x| x.cmp(&10)), 3..4);
+        (11..=14).for_each(|i| assert_eq!(bit.find_range_by(|x| x.cmp(&i)), 4..4));
+        assert_eq!(bit.find_range_by(|x| x.cmp(&15)), 4..5);
+        (16..=20).for_each(|i| assert_eq!(bit.find_range_by(|x| x.cmp(&i)), 5..5));
+        assert_eq!(bit.find_range_by(|x| x.cmp(&21)), 5..6);
+        (22..=26).for_each(|i| assert_eq!(bit.find_range_by(|x| x.cmp(&i)), 6..6));
+        assert_eq!(bit.find_range_by(|x| x.cmp(&27)), 6..7);
+        (28..=34).for_each(|i| assert_eq!(bit.find_range_by(|x| x.cmp(&i)), 7..7));
+        assert_eq!(bit.find_range_by(|x| x.cmp(&35)), 7..8);
+        (36..=43).for_each(|i| assert_eq!(bit.find_range_by(|x| x.cmp(&i)), 8..8));
+        assert_eq!(bit.find_range_by(|x| x.cmp(&44)), 8..9);
+        (45..=53).for_each(|i| assert_eq!(bit.find_range_by(|x| x.cmp(&i)), 9..9));
+        assert_eq!(bit.find_range_by(|x| x.cmp(&54)), 9..10);
+        (55..100).for_each(|i| assert_eq!(bit.find_range_by(|x| x.cmp(&i)), 10..10));
+    }
+
+    #[test]
+    fn partition_point() {
+        let bit: FenwickTree<AddMonoid<_>> = vec![1, 2, 3, 4, 5, 6, 6, 8, 9, 10].into();
+
+        assert_eq!(bit.partition_point(|&x| x <= 20), 5);
+        assert_eq!(bit.partition_point(|&x| x <= 21), 6);
     }
 }
