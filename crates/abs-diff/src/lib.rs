6f84eff7cdc785b54e69285d1ce03cdb6758150e
SGThr7 <sgthr7@gmail.com>
2022-09-03 12:03:16 +0900

    refactor: Add test-case to abs-diff

--- crates/abs-diff/src/lib.rs
+++ crates/abs-diff/src/lib.rs
@@ -17,6 +17,7 @@ pub trait AbsDiff<Rhs = Self> {
     /// use abs_diff::AbsDiff;
     ///
     /// assert_eq!(10.abs_diff1(20), 10);
+    /// assert_eq!(20.abs_diff1(10), 10);
     /// assert_eq!(100.abs_diff1(20), 80);
     /// ```
     fn abs_diff1(self, other: Self) -> Self;
@@ -39,3 +40,42 @@ macro_rules! impl_abs_diff {
 }
 
 impl_abs_diff! { u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize f32 f64 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bounds() {
+        let max = isize::MAX;
+        let zero = 0;
+        let min = isize::MIN;
+
+        assert_eq!(max.abs_diff1(zero), max);
+        assert_eq!(zero.abs_diff1(max), max);
+
+        assert_eq!((min + 1).abs_diff1(zero), max);
+        assert_eq!(zero.abs_diff1(min + 1), max);
+
+        assert_eq!(min.abs_diff1(zero - 1), max);
+        assert_eq!((zero - 1).abs_diff1(min), max);
+    }
+
+    #[test]
+    fn plus() {
+        assert_eq!(57.abs_diff1(7), 50);
+        assert_eq!(7.abs_diff1(57), 50);
+    }
+
+    #[test]
+    fn minus() {
+        assert_eq!((-57).abs_diff1(-7), 50);
+        assert_eq!((-7).abs_diff1(-57), 50);
+    }
+
+    #[test]
+    fn plus_minus() {
+        assert_eq!(10.abs_diff1(-10), 20);
+        assert_eq!((-10).abs_diff1(10), 20);
+    }
+}
